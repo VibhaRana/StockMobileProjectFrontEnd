559b2614863a4c98f5e4f9aa11e26a6988a7b743
xdc811 <xdc811@users.noreply.github.com>
2020-03-28 22:32:30 -0700

    Search

--- auth/AuthAPIClass.js
+++ auth/AuthAPIClass.js
@@ -3,14 +3,14 @@ import { AsyncStorage } from 'react-native';
 
 
 var instance = axios.create({
-    baseURL: 'http://192.168.12.107:5000/'
+    baseURL: 'http://192.168.1.100:5000/'
 });
 
 const DataAccessService = {
     async login(username, password) {
         if(username==null||username.length<3){
             console.log("using test account")
-            username="test@test.com"
+            username="test1@test.com"
             password= "P@ssw0rd"
         }
         var result = await instance.post('api/login', {
@@ -19,6 +19,7 @@ const DataAccessService = {
             RemenberMe: false
         })
         if (result.status == 200 && result.data.status == 200) {
+            console.log(result.data.token)
             return {status:200,token:result.data.token};
         } else {
             return { status: 400 }
@@ -62,6 +63,12 @@ const DataAccessService = {
     async SignUp(username, password, comfirmPassword) {
         console.log("Sign up!!!!")
         // let result = await 
+        if(username==null||username.length<3){
+            console.log("using test account")
+            username="test1@test.com"
+            password= "P@ssw0rd"
+            comfirmPassword= "P@ssw0rd"
+        }
        var result=await instance.post('api/register', {
             "email": username,
             "password": password,
@@ -84,7 +91,108 @@ const DataAccessService = {
         console.log("=======final=======")
         console.log(result)
         return result
+    },
+    async getPerformance(){
+        try {
+            let token = await AsyncStorage.getItem("userToken")
+            if (token != null) {
+                token = `Bearer ${token}`
+                console.log(token)
+                let result = await instance.get("api/Performance", {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': token
+                    }
+                }).catch((e) => {
+                    console.log(e.response.data)
+                    console.log(e.response.headers)
+                    console.log(e.response.status)
+                    return e.response.headers
+                })
+                console.log("=======================================")
+                console.log(result.data)
+                return result
+
+            }
+        } catch (e) {
+
+        }
+        return "error!!!!!!"
+    },
+    async postPerformance(){
+        try {
+            let token = await AsyncStorage.getItem("userToken")
+            if (token != null) {
+                token = `Bearer ${token}`
+                console.log( token)
+                let result = await instance.post("api/Performance",{
+                    "startDate": "2020-03-28T21:35:55.8984115",
+                    "cash": 1000000.0,
+                    "performance": "12322,12332,33223232"
+                }, {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Authorization': token
+                    }
+                }).catch((e) => {
+                    console.log("Error!!!")
+                    console.log(e.response)
+                    console.log(e.response.headers)
+                    console.log(e.response.status)
+                    return e.response.headers
+                })
+                console.log("=======================================")
+                console.log(result)
+                return result
+
+            }
+        } catch (e) {
+
+        }
+        return "error!!!!!!"
+    },
+    async getWatchList() {
+        let token = await AsyncStorage.getItem('userToken');
+        let response = await instance.get('api/watched', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        if (response.status == 200) {
+            return { status: 200, stocks: response.data.stocks };
+        } else {
+            return { status: response.status, detail: response.data.detail };
+        }
+    },
+    async setWatch(symbol, isWatch) {
+        let token = await AsyncStorage.getItem('userToken');
+        console.log(token);
+        console.log(symbol, isWatch);
+        let response = await instance.put('api/watch',
+        {
+            'Symbol': symbol,
+            'IsWatch': isWatch
+        } ,{
+           
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        if (response.status == 200) {
+            return { status: 200 };
+        } else {
+            return { status: response.status, detail: response.data.detail };
+        }
     }
+    
 };
 
 export default DataAccessService;
--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text,Button } from 'react-native';
 import FinnhubAPI from "../auth/Finnhub"
 import Autocomplete from 'react-native-autocomplete-input';
 export default function SearchScreen({ navigation, route}) {
-    const {symbol,name}=route.params
+    const {data}=route.params
+    
+    const [currentPrice, setCurrentPrice] = useState("");
+    useEffect( () => {
+      const start=async()=>{
+        let temp = await FinnhubAPI.getQuote(data);
+        setCurrentPrice(temp.c);
+      }
+      start()
+     
+    },[])
   return (
     <View>
       <Text>detail Screen</Text>
-      <Text>{symbol}</Text>
-      <Text>{name}</Text>
-      <Button onPress={()=>{FinnhubAPI.getQuote("AAPL")}} title="search"></Button>
+      <Text>current price :{currentPrice}</Text>
+      <Text>{data}</Text>
+     
      
     </View>
   );
