bcec5a1cae02cca85e6892cca66ee0c422bca887
Jinhong Liang <liangjinhongwin@hotmail.com>
2020-03-30 23:49:19 -0700

    Added keyboardavoid

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,120 +1,131 @@
-import React, { useState, useEffect } from 'react';
-import { ScrollView, Text, Button, TextInput, SafeAreaView } from 'react-native';
+import React, { useState, useEffect, version } from 'react';
+import { ScrollView, Text, Button, TextInput, SafeAreaView, KeyboardAvoidingView, Platform, Keyboard, TouchableWithoutFeedback, View } from 'react-native';
 import FinnhubAPI from "../auth/Finnhub";
 import AuthAPIClass from '../auth/AuthAPIClass';
 import NumberFormat from 'react-number-format';
 
 export default function DetailScreen({ route }) {
-    const { data } = route.params
-    const [countInput, setCountInput] = useState("");
-    const [price, setPrice] = useState({
-        open: "",
-        high: "",
-        low: "",
-        currentPrice: "",
-        previousClose: ""
-    });
-    const [profile, setProfile] = useState({
-        name: "",
-        marketCapitalization: "",
-        shareOutstanding: "",
-        description: ""
-    });
-    const [response, setResponse] = useState({
-        detail: "",
-        currentCash: "",
-        remaining: 0,
-        totalPurchased: 0
-    });
-    useEffect(() => {
-        const getData = async () => {
-            const tempQuote = await FinnhubAPI.getQuote(data);
-            const tempProfile = await FinnhubAPI.getProfile(data);
-            setPrice({
-                open: tempQuote.o,
-                high: tempQuote.h,
-                low: tempQuote.l,
-                currentPrice: tempQuote.c,
-                previousClose: tempQuote.pc
-            });
-            setProfile({
-                name: tempProfile.name,
-                marketCapitalization: (tempProfile.marketCapitalization * 1000000),
-                shareOutstanding: (tempProfile.shareOutstanding * 1000000),
-                description: tempProfile.description
-            });
-        };
-        getData();
-    }, []);
-    async function callBuy(symbol, count, price) {
-        let boughtData = await AuthAPIClass.buy(symbol, count, price)
-        setResponse(boughtData);
+  const { data } = route.params
+  const [countInput, setCountInput] = useState("");
+  const [price, setPrice] = useState({
+    open: "",
+    high: "",
+    low: "",
+    currentPrice: "",
+    previousClose: ""
+  });
+  const [profile, setProfile] = useState({
+    name: "",
+    marketCapitalization: "",
+    shareOutstanding: "",
+    description: ""
+  });
+  const [response, setResponse] = useState({
+    detail: "",
+    currentCash: "",
+    remaining: 0,
+    totalPurchased: 0
+  });
+  useEffect(() => {
+    const getData = async () => {
+      const tempQuote = await FinnhubAPI.getQuote(data);
+      const tempProfile = await FinnhubAPI.getProfile(data);
+      setPrice({
+        open: tempQuote.o,
+        high: tempQuote.h,
+        low: tempQuote.l,
+        currentPrice: tempQuote.c,
+        previousClose: tempQuote.pc
+      });
+      setProfile({
+        name: tempProfile.name,
+        marketCapitalization: (tempProfile.marketCapitalization * 1000000),
+        shareOutstanding: (tempProfile.shareOutstanding * 1000000),
+        description: tempProfile.description
+      });
     };
-    async function callSell(symbol, count, price) {
-        let soldData = await AuthAPIClass.sell(symbol, count, price)
-        setResponse(soldData);
-    };
-    async function callAddToWatch(symbol) {
-        await AuthAPIClass.setWatch(symbol, true);
-        setResponse({
-            detail: "Added to watchlist"
-        })
-    }
-    return (
-        <SafeAreaView>
-            <ScrollView>
-                <Text>Name: {profile.name}</Text>
-                <Text>Symbol: {data}</Text>
-                <Text>
-                    Market Capitalization:{' '}
-                    <NumberFormat
-                        value={profile.marketCapitalization}
-                        displayType={'text'}
-                        thousandSeparator={true}
-                        renderText={value => <Text>{value}</Text>}
-                    />
-                </Text>
-                <Text>
-                    Shares Outstanding:{' '}
-                    <NumberFormat
-                        value={profile.shareOutstanding}
-                        displayType={'text'}
-                        thousandSeparator={true}
-                        renderText={value => <Text>{value}</Text>}
-                    />
-                </Text>
-                <Text>{profile.description}</Text>
-                <Text>Current Price: ${price.currentPrice}</Text>
-                <Text>Open Price: ${price.open}</Text>
-                <Text>High Price: ${price.high}</Text>
-                <Text>Low Price: ${price.low}</Text>
-                <Text>Previous Close Price: ${price.previousClose}</Text>
-                <TextInput
-                    placeholder={"Enter quantity here"}
-                    onChangeText={(text) => setCountInput(text)}
-                    value={countInput}
-                    keyboardType={'numeric'}
-                />
-                <Button
-                    title="Buy"
-                    onPress={() => callBuy(data, countInput, price.currentPrice)}
-                />
-                <Button
-                    title="Sell"
-                    onPress={() => callSell(data, countInput, price.currentPrice)}
-                />
-                <Button
-                    title="Watch"
-                    onPress={() => callAddToWatch(data)}
-                />
-                <Text>{(response.detail != "") ? response.detail : ""}</Text>
-                <Text>
-                    {(response.remaining != "" && response.remaining != null) ? "You have " + response.remaining + " stocks remaining." : (response.totalPurchased != "" && response.totalPurchased != null) ? "You currently have " + response.totalPurchased + " stocks purchased." : ""}
-                </Text>
-                <Text>
-                    {(response.currentCash != "" && response.currentCash != null) ? "Your current cash amount is: $" + response.currentCash : ""}
-                </Text>
-            </ScrollView>
-        </SafeAreaView>
-    );
+    getData();
+  }, []);
+  async function callBuy(symbol, count, price) {
+    let boughtData = await AuthAPIClass.buy(symbol, count, price)
+    setResponse(boughtData);
+  };
+  async function callSell(symbol, count, price) {
+    let soldData = await AuthAPIClass.sell(symbol, count, price)
+    setResponse(soldData);
+  };
+  async function callAddToWatch(symbol) {
+    await AuthAPIClass.setWatch(symbol, true);
+    setResponse({
+      detail: "Added to watchlist"
+    })
+  }
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.Os == "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+          <ScrollView>
+            <Text>Name: {profile.name}</Text>
+            <Text>Symbol: {data}</Text>
+            <Text>
+              Market Capitalization:{' '}
+              <NumberFormat
+                value={profile.marketCapitalization}
+                displayType={'text'}
+                thousandSeparator={true}
+                renderText={value => <Text>{value}</Text>}
+              />
+            </Text>
+            <Text>
+              Shares Outstanding:{' '}
+              <NumberFormat
+                value={profile.shareOutstanding}
+                displayType={'text'}
+                thousandSeparator={true}
+                renderText={value => <Text>{value}</Text>}
+              />
+            </Text>
+            <Text>{profile.description}</Text>
+            <Text>Current Price: ${price.currentPrice}</Text>
+            <Text>Open Price: ${price.open}</Text>
+            <Text>High Price: ${price.high}</Text>
+            <Text>Low Price: ${price.low}</Text>
+            <Text>Previous Close Price: ${price.previousClose}</Text>
+            <TextInput
+              placeholder={"Enter quantity here"}
+              onChangeText={(text) => setCountInput(text)}
+              value={countInput}
+              keyboardType={'numeric'}
+            />
+            <View>
+              <Button
+                title="Buy"
+                onPress={() => callBuy(data, countInput, price.currentPrice)}
+              />
+              <Button
+                title="Sell"
+                onPress={() => callSell(data, countInput, price.currentPrice)}
+              />
+              <Button
+                title="Watch"
+                onPress={() => callAddToWatch(data)}
+              />
+            </View>
+          </ScrollView>
+          <View style={{ marginBottom: 20 }} >
+            <Text>{(response.detail != "") ? response.detail : ""}</Text>
+            <Text>
+              {(response.remaining != "" && response.remaining != null) ? "You have " + response.remaining + " stocks remaining." : (response.totalPurchased != "" && response.totalPurchased != null) ? "You currently have " + response.totalPurchased + " stocks purchased." : ""}
+            </Text>
+            <Text>
+              {(response.currentCash != "" && response.currentCash != null) ? "Your current cash amount is: $" + response.currentCash : ""}
+            </Text>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
 }
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { AsyncStorage, TouchableOpacity, Text, TextInput, View, StyleSheet, SafeAreaView } from 'react-native';
+import { AsyncStorage, TouchableOpacity, Text, TextInput, View, StyleSheet, KeyboardAvoidingView, Keyboard, Platform, TouchableWithoutFeedback } from 'react-native';
 import { AuthContext } from "../auth/authContext"
 import { getCurrentFrame } from 'expo/build/AR';
 
@@ -11,78 +11,82 @@ export default function SignUpScreen() {
 
   return (
 
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Welcome to Best Brokers</Text>
-      <TextInput
-        style={styles.input}
-        underlineColorAndroid='rgba(0,0,0,0)'
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        style={styles.input}
-        underlineColorAndroid='rgba(0,0,0,0)'
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <TextInput
-        style={styles.input}
-        underlineColorAndroid='rgba(0,0,0,0)'
-          placeholder="Comfirm Password"
-          value={comfirmPassword}
-          onChangeText={setComfirmPassword}
-          secureTextEntry
-        />
-        <TouchableOpacity title="Sign up" onPress={() => signUp({ username, password,comfirmPassword })} />
-         <TouchableOpacity style={styles.button}>
-         <Text style={styles.buttonText}>SignUp</Text>
-         </TouchableOpacity>
-      </SafeAreaView>
-    );
-  }
-   const styles = StyleSheet.create({
-     container: {
-       backgroundColor: '#009688',
-       flex: 1,
-       alignItems: 'center',
-       justifyContent: 'center',
-       
-     
-     },
-     input:{
-       width: 300,
-       backgroundColor: 'rgba(255, 255, 255, 0.3)',
-       borderRadius: 25,
-       paddingHorizontal: 19,
-       paddingVertical: 12,
-       marginVertical: 10,
-       
-     },
-     button: {
-       width:300,
-       backgroundColor: '#fc5c65',
-       borderRadius: 25,
-       marginVertical: 10,
-       paddingVertical: 12,
-       shadowColor: 'black'
-     },
-     buttonText: {
-       fontSize: 16,
-       fontWeight: '500',
-       color: '#ffffff',
-       marginVertical: 10,
-       textAlign: 'center'
-     },
-     title:{
-       color: 'white',
-       fontWeight: 'bold',
-       fontSize: 20,
-       shadowColor: 'black',
-       elevation: 5
-     },
+    <KeyboardAvoidingView
+      behavior={Platform.Os == "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <Text style={styles.title}>Welcome to Best Brokers</Text>
+          <TextInput
+            style={styles.input}
+            underlineColorAndroid='rgba(0,0,0,0)'
+            placeholder="Username"
+            value={username}
+            onChangeText={setUsername}
+          />
+          <TextInput
+            style={styles.input}
+            underlineColorAndroid='rgba(0,0,0,0)'
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+          <TextInput
+            style={styles.input}
+            underlineColorAndroid='rgba(0,0,0,0)'
+            placeholder="Comfirm Password"
+            value={comfirmPassword}
+            onChangeText={setComfirmPassword}
+            secureTextEntry
+          />
+          <TouchableOpacity title="Sign up" onPress={() => signUp({ username, password, comfirmPassword })} />
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>SignUp</Text>
+          </TouchableOpacity>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#009688',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width: 300,
+    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    borderRadius: 25,
+    paddingHorizontal: 19,
+    paddingVertical: 12,
+    marginVertical: 10,
+  },
+  button: {
+    width: 300,
+    backgroundColor: '#fc5c65',
+    borderRadius: 25,
+    marginVertical: 10,
+    paddingVertical: 12,
+    shadowColor: 'black'
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#ffffff',
+    marginVertical: 10,
+    textAlign: 'center'
+  },
+  title: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+    shadowColor: 'black',
+    elevation: 5
+  },
   button: {
     width: 300,
     backgroundColor: '#fc5c65',
